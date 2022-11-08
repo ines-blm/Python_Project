ffd9b6d8ac5556ec895ad0daca5a30082716a168
ines1987 <117329521+ines1987@users.noreply.github.com>
2022-11-08 17:14:42 -0500

    first commit

---
+++ max _min_project.py
@@ -0,0 +1,9 @@
+list1=[3, 4, 5, 6]
+list2=[-1, -2, -3, -4]
+list3=[0, 0, 0, 0]
+list4=[]
+
+print(f"{max(list1)} {min(list1)}\n{max(list2)} {min(list2)}\n{max(list3)} {min(list3)}")
+if len(list4) == 0:
+  print("none")
+  
