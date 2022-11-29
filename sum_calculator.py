a14354e7caaca2546f07fb76b832aab35366673e
ines-blm <117329521+ines-blm@users.noreply.github.com>
2022-11-29 18:08:04 -0500

    first commit

---
+++ sum_calculator.py
@@ -0,0 +1,4 @@
+number1= int(input ("enter the firstnumber:\n"))
+number2= int(input("enter the secondnumber:\n"))
+sum = int(number1) + int(number2)
+print (f"the sum of { number1} and {number2} is: {sum}")
