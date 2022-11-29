87301d3645bc30f88cc901e4a5ea2045bbcb54c6
ines-blm <117329521+ines-blm@users.noreply.github.com>
2022-11-29 18:01:07 -0500

    first commit

---
+++ countdown.py
@@ -0,0 +1,10 @@
+# this Code count down backward from 10 to 0
+limit = 11
+count = 0
+count_down = 10
+while count < limit:
+  count += 1
+  print (count_down)
+  count_down -= 1
+  continue
+print("Countdown is over")
