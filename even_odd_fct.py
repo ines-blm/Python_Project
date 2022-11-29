5c7a9106e6d49246a1ab3f1affbf2b5829305c69
ines-blm <117329521+ines-blm@users.noreply.github.com>
2022-11-29 17:45:33 -0500

    first commit

---
+++ even_odd_fct.py
@@ -0,0 +1,13 @@
+# even and odd number using function
+number1 = int(input("enter your number:\n"))
+
+
+def even_odd_fct(number):
+    if number % 2 == 0: 
+        print(f'{number} is even')
+
+    else: 
+        print(f'{number} is odd')
+
+even_odd_fct(number1)
+
